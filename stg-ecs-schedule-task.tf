d1e3ba96acd0bc6746af3af357d6a996e16a9ca3
Matsushin <shinichi.matsuda@matsushin.tech>
2023-03-16 14:34:39 +0900

    バッチが動くように

--- stg-ecs-schedule-task.tf
+++ stg-ecs-schedule-task.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "stg-summarize_daily_client_information" {
   name                  = "stg-summarize_daily_client_information"
   description           = "stg-summarize_daily_client_information"
-  schedule_expression   = "cron(30 23 * * ? *)"
+  schedule_expression   = "cron(45 23 * * ? *)"
 }
  
 resource "aws_cloudwatch_event_target" "stg-summarize_daily_client_information" {
@@ -34,7 +34,7 @@ resource "aws_cloudwatch_event_target" "stg-summarize_daily_client_information" 
         aws_security_group.allow_api.id,
         aws_security_group.allow_all_outbound.id
       ]
-      assign_public_ip        = false
+      assign_public_ip        = true
     }
   }
 }
